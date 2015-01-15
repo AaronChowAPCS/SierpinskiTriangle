21cd783bca92b905331954e4929343116207cb6e
aaronchowapcs <aahchow@gmail.com>
2015-01-15 11:35:29 -0800

    Added Triangle Pyramid.

--- build-tmp/source/SierpinskiTriangle.java
+++ build-tmp/source/SierpinskiTriangle.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
 //color myColor = color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
 int limit  = 20;
 public void setup()
@@ -30,4 +46,13 @@ public void sierpinski(int x, int y, int len)
 	else{
 		triangle(x, y, x + len/2, y - len, x + len, y);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
