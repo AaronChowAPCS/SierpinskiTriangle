75aceeb0fc76530f19ac01c57c6b9770ef25730e
aaronchowapcs <aahchow@gmail.com>
2015-01-20 11:39:38 -0800

    Added variable colors and number of triangles.

--- build-tmp/source/SierpinskiTriangle.java
+++ build-tmp/source/SierpinskiTriangle.java
@@ -14,33 +14,60 @@ import java.io.IOException;
 
 public class SierpinskiTriangle extends PApplet {
 
-//color myColor = color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
 int limit  = 20;
+int colorOne = (int)(Math.random()*255);
+int colorTwo = (int)(Math.random()*255);
+int colorThree = (int)(Math.random()*255);
+int shrinkOne = 5;
+int shrinkTwo = 5;
+int shrinkThree = 5;
+
 public void setup()
 {
 	background(0);
 	size(1000,1000);
-	//fill(myColor);
-	
 }
 public void draw()
 {
-	//myColor = color((int)(Math.random()*255));
-	//fill(myColor);
-	//tint(255,100);
+	if(colorOne >= 255 && shrinkOne == 5)
+		shrinkOne *= -1;
+	else if(colorOne <= 5)
+		shrinkOne *= -1;
+	colorOne += shrinkOne;
+	if(colorTwo >= 255 && shrinkTwo == 5)
+		shrinkTwo *= -1;
+	else if(colorTwo <= 5)
+		shrinkTwo *= -1;
+	colorTwo += shrinkTwo;
+	if(colorThree >= 255 && shrinkThree == 5)
+		shrinkThree *= -1;
+	else if(colorThree <= 5)
+		shrinkThree *= -1;
+	colorThree += shrinkThree;
 	sierpinski(100,900,800);
-
-	//triangle(100,400,400,400,250,100);
 }
-public void mousePressed()//optional
+
+public void keyPressed()
 {
-	limit -= 5;
+	if(key == 'a'){
+		if(limit <= 10)
+			limit *= 2;
+		limit /= 2;
+	}
+	if(key == 'd'){
+		if(limit >= 400)
+			limit /= 2;
+		limit *= 2;
+	}
 }
 public void sierpinski(int x, int y, int len) 
 {
 	if(len >= limit){
+		fill(colorOne,colorTwo,0);
 		sierpinski(x, y, len / 2);
+		fill(0,colorTwo,colorThree);
 		sierpinski(x + len /2, y, len / 2);
+		fill(colorOne,0,colorThree);
 		sierpinski( x + len / 4, y - len / 2, len / 2);
 	}
 	else{
